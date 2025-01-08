3d1ae548d0d0c00d913a0e6225e9b7a70dc5c179
Engr. Shahsawar Khanzada <shahsawarkhanzada@gmail.com>
2025-01-08 16:16:50 +0500

    logic for uploading data to the database created

--- lib/View/addPost.dart
+++ lib/View/addPost.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
 import 'package:blog_app/Components/RoundedButton.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:firebase_database/firebase_database.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -12,6 +15,12 @@ class AddPostScreen extends StatefulWidget {
 }
 
 class _AddPostScreenState extends State<AddPostScreen> {
+  final blogRef = FirebaseDatabase.instance.ref().child('Blogs');
+
+  firebase_storage.FirebaseStorage storage =
+      firebase_storage.FirebaseStorage.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
+
   File? image;
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
@@ -148,7 +157,44 @@ class _AddPostScreenState extends State<AddPostScreen> {
               SizedBox(
                 height: height * 0.1,
               ),
-              RoundedButton(title: 'Upload', onPress: () {})
+              RoundedButton(
+                  title: 'Upload',
+                  onPress: () async {
+                    try {
+                      int date = DateTime.now().microsecondsSinceEpoch;
+
+                      firebase_storage.Reference ref = firebase_storage
+                          .FirebaseStorage.instance
+                          .ref('/blog_app_with_firebase $date');
+                      firebase_storage.UploadTask uploadTask =
+                          ref.putFile(image!.absolute);
+
+                      await Future.value(uploadTask);
+                      var newUrl = await ref.getDownloadURL();
+
+                      final user = auth.currentUser;
+
+                      blogRef
+                          .child('Blog List')
+                          .child(date.toString())
+                          .set({'blogId': date.toString(), 
+                          'blogImage' : newUrl.toString(),
+                          'blogTime' : date.toString(),
+                          'blogTitle' : titleController.text.toString(),
+                          'blogDescription' : descriptionController.text.toString(),
+                          'userEmail' : user!.email.toString(),
+                          'userId' : user.uid.toString()}).then((value) {
+                                                  ScaffoldMessenger.of(context)
+                          .showSnackBar(const SnackBar(content: Text('Blog has been published')));
+                          }).onError((error, stackTrace) {
+                                                  ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(error.toString())));
+                          });
+                    } catch (e) {
+                      ScaffoldMessenger.of(context)
+                          .showSnackBar(SnackBar(content: Text(e.toString())));
+                    }
+                  })
             ],
           ),
         ),
