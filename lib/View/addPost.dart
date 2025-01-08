7a069ef2c5d7c0a72ac6f411ff683e696ff1817f
Engr. Shahsawar Khanzada <shahsawarkhanzada@gmail.com>
2025-01-08 13:00:29 +0500

    Image picker functionality added in AddPost screen

--- lib/View/addPost.dart
+++ lib/View/addPost.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:blog_app/Components/RoundedButton.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddPostScreen extends StatefulWidget {
   const AddPostScreen({super.key});
@@ -14,6 +15,31 @@ class _AddPostScreenState extends State<AddPostScreen> {
   File? image;
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
+  final picker = ImagePicker();
+
+  Future getgalleryImage() async {
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedImage != null) {
+        image = File(pickedImage.path);
+      } else {
+        print('No Image selected');
+      }
+    });
+  }
+
+  Future getCameraImage() async {
+    final pickedImage = await picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedImage != null) {
+        image = File(pickedImage.path);
+      } else {
+        print('No image selected');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.sizeOf(context).height * 1;
@@ -29,31 +55,70 @@ class _AddPostScreenState extends State<AddPostScreen> {
           padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
           child: Column(
             children: [
-              Center(
-                child: Container(
-                  height: height * 0.2,
-                  width: width,
-                  child: image != null
-                      ? ClipRRect(
-                          child: Image.file(
-                            image!.absolute,
+              InkWell(
+                onTap: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          content: SizedBox(
+                            height: 120,
+                            child: Column(
+                              children: [
+                                InkWell(
+                                  onTap: () {
+                                    getCameraImage();
+                                    Navigator.pop(context);
+                                  },
+                                  child: const ListTile(
+                                    leading: Icon(Icons.camera_alt_outlined),
+                                    title: Text('Camera'),
+                                  ),
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    getgalleryImage();
+                                    Navigator.pop(context);
+                                  },
+                                  child: const ListTile(
+                                    leading: Icon(Icons.photo_library),
+                                    title: Text('Gallery'),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      });
+                },
+                child: Center(
+                  child: Container(
+                    height: height * 0.2,
+                    width: width,
+                    child: image != null
+                        ? ClipRRect(
+                            child: Image.file(
+                              image!.absolute,
+                              height: 100,
+                              width: 100,
+                              fit: BoxFit.cover,
+                            ),
+                          )
+                        : Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(10),
+                            ),
                             height: 100,
                             width: 100,
-                            fit: BoxFit.fitHeight,
+                            child: const Icon(
+                              Icons.camera_alt,
+                              color: Colors.blue,
+                            ),
                           ),
-                        )
-                      : Container(
-                          decoration: BoxDecoration(
-                            color: Colors.grey[200],
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          height: 100,
-                          width: 100,
-                          child: const Icon(
-                            Icons.camera_alt,
-                            color: Colors.blue,
-                          ),
-                        ),
+                  ),
                 ),
               ),
               SizedBox(
